1cccd4b6fa174d1fe2456357cc07289a26f99569
sergey <sergey.panin.2019@gmail.com>
2020-03-06 09:33:10 +0300

    get pictures

--- Main.py
+++ Main.py
@@ -1,17 +1,21 @@
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
-
+from telegram.ext import CommandHandler, MessageHandler
+from telegram.ext import Filters
 
 Token = '506889620:AAEu2LhOhwYf0jcLLPnX2v3t0p38679198o'
 
-updater = Updater(Token=Token, use_context=True)
+updater = Updater(token=Token, use_context=True)
 dispatcher = updater.dispatcher
 
 def Start(update, context):
     print('fffff')
 Start_Handler = CommandHandler('start', Start)
 
+def Get_Image(updater, context):
+    print('i get img')
+Get_Image_Handler = MessageHandler(Filters.document.category('image'), Get_Image)
 
 dispatcher.add_handler(Start_Handler)
+dispatcher.add_handler(Get_Image_Handler)
 
-update.start_pulling()
+updater.start_polling()
