e777a89e1ca5fdd3ddc142647934a6b88ed60fe7
sergey <sergey.panin.2019@gmail.com>
2020-03-08 23:21:48 +0300

    add identification car nummber

--- Main.py
+++ Main.py
@@ -2,12 +2,21 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler
 from telegram.ext import Filters
 import logging
+
+import requests
+import base64
+import json
+
 import os
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
 Token = '506889620:AAEu2LhOhwYf0jcLLPnX2v3t0p38679198o'
+
+IMAGE_PATH = r'temp.jpeg'
+SECRET_KEY = r'sk_DEMODEMODEMODEMODEMODEMO'
+URL = r'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=ru&secret_key=%s' % (SECRET_KEY)
 
 updater = Updater(token=Token, use_context=True)
 dispatcher = updater.dispatcher
@@ -22,8 +31,17 @@ Get_Image_Handler = MessageHandler(Filters.document.category('image'), Get_Image
 
 def Get_Photo(update, context):
     print('i get photo')
-    update.message.photo[-1].get_file().download()
+    update.message.photo[-1].get_file().download(custom_path = 'temp.jpeg')
     os.system('ls')
+    
+    with open(IMAGE_PATH, 'rb') as image_file:
+        img_base64 = base64.b64encode(image_file.read())
+    r = requests.post(URL, data = img_base64)
+    s = json.dumps(r.json(), indent=3)
+    print(json.loads(s).get("results")[0].get('plate'))
+
+
+
 Get_Photo_Handler = MessageHandler(Filters.photo, Get_Photo)
 
 dispatcher.add_handler(Start_Handler)
