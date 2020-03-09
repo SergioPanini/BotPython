d394633e66740018c8020355fd75be053c54bb96
sergey <sergey.panin.2019@gmail.com>
2020-03-09 08:56:38 +0300

    create db

--- testDB.py
+++ testDB.py
@@ -2,7 +2,7 @@ import sqlite3
 
 DB_URL = r'db.sqlite3'
 
-conn = sqlite3.connect(DB_URL)
+conn = sqlite3.connect(":memory:", check_same_thread = False)
 cour = conn.cursor()
 
 result = cour.execute("CREATE TABLE main_table(id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(255), Surname VARCHAR(255), Number VARCHR(255), Card VARCHAR(255));")
