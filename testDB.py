8ab5e9e5a8ec7c23c24e726d2de4a9d8e6c74e9f
sergey <sergey.panin.2019@gmail.com>
2020-03-09 08:55:30 +0300

    create db

---
+++ testDB.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+DB_URL = r'db.sqlite3'
+
+conn = sqlite3.connect(DB_URL)
+cour = conn.cursor()
+
+result = cour.execute("CREATE TABLE main_table(id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(255), Surname VARCHAR(255), Number VARCHR(255), Card VARCHAR(255));")
+print(result)
