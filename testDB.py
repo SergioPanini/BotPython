e532ba4efadf6140a14acc1890208d1bd4b98949
sergey <sergey.panin.2019@gmail.com>
2020-03-11 00:09:00 +0300

    test db

--- testDB.py
+++ testDB.py
@@ -2,8 +2,10 @@ import sqlite3
 
 DB_URL = r'db.sqlite3'
 
-conn = sqlite3.connect(":memory:", check_same_thread = False)
+#conn = sqlite3.connect(":memory:", check_same_thread = False)
+conn = sqlite3.connect(DB_URL)
+
 cour = conn.cursor()
 
-result = cour.execute("CREATE TABLE main_table(id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(255), Surname VARCHAR(255), Number VARCHR(255), Card VARCHAR(255));")
-print(result)
+result = cour.execute("SELECT * FROM main_table")
+print(result.fetchall())
